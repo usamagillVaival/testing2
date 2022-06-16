aa1b50814d5e74a92e3531b623e52d335c2b76d3
usamagillVaival <84457170+usamagillVaival@users.noreply.github.com>
2022-06-16 15:15:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
+require("dotenv").config();
+
+
+// app
+const app = express();
+
+// middlewares
+app.use(cors());
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator()); 
+app.use(cors());
+
+
+// import routes
+const userRoutes = require("./routes/user");
+
+// app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+
+const port = 8002;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
